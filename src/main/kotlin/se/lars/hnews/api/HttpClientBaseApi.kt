ea052f200330f2a5c61689afbc2ebaee8b6e6f4f
Lars Krog-Jensen <lars.krogjensen@gmail.com>
2017-12-14 19:55:46 +0100

    replaced httpclient with webclient

--- src/main/kotlin/se/lars/hnews/api/HttpClientBaseApi.kt
+++ src/main/kotlin/se/lars/hnews/api/HttpClientBaseApi.kt
@@ -2,9 +2,13 @@ package se.lars.hnews.api
 
 import io.netty.handler.codec.http.HttpResponseStatus
 import io.vertx.core.Vertx
+import io.vertx.core.buffer.Buffer
 import io.vertx.core.http.HttpClient
 import io.vertx.core.http.HttpClientOptions
 import io.vertx.core.http.HttpVersion
+import io.vertx.ext.web.client.HttpResponse
+import io.vertx.ext.web.client.WebClient
+import io.vertx.ext.web.client.WebClientOptions
 import se.lars.hnews.defaultMapper
 import se.lars.kutil.loggerFor
 import java.util.concurrent.CompletableFuture
@@ -18,6 +22,7 @@ open class HttpClientBaseApi
     protected val log = loggerFor<HackerNewsApi>()
     protected val httpClient: HttpClient
     protected val mapper = defaultMapper
+    protected val webClient: WebClient
 
     init {
 
@@ -34,37 +39,49 @@ open class HttpClientBaseApi
 
         // Http client is thread safe an a single instance is sufficent
         httpClient = vertx.createHttpClient(options)
+
+        val wcOptions = WebClientOptions().apply {
+            protocolVersion = HttpVersion.HTTP_2
+            isSsl = true
+            isUseAlpn = true
+            defaultHost = baseUrl
+            defaultPort = 443
+            logActivity = true
+            connectTimeout = 1000
+        }
+        webClient = WebClient.create(vertx, wcOptions)
     }
 
     inline protected fun <reified T : Any> invokeQuery(query: String): CompletableFuture<T> {
         val future = CompletableFuture<T>()
 
-        log.info("Query: ${formatUrl(query)}")
-
-        httpClient.get(query)
-            .setTimeout(30_000)
-            .exceptionHandler { ex -> future.completeExceptionally(ex) }
-            .handler { response ->
-                if (response.statusCode() == HttpResponseStatus.OK.code()) {
-                    log.info("Protocol version: ${response.version()}")
-                    response.bodyHandler { buffer ->
+        webClient.get(query)
+            .send { queryResult ->
+                if (queryResult.succeeded()) {
+                    val response: HttpResponse<Buffer> = queryResult.result()
+                    if (response.statusCode() == HttpResponseStatus.OK.code()) {
+                        log.info("Protocol version: ${response.version()}")
                         try {
-                            val typeObj = mapper.readValue(buffer.bytes, T::class.java)
+                            val typeObj = mapper.readValue(response.body().bytes, T::class.java)
                             future.complete(typeObj)
                         } catch (e: Exception) {
                             log.error("Failed to invoke ${formatUrl(query)}", e)
                             future.completeExceptionally(e)
                         }
+                    } else if (response.statusCode() == HttpResponseStatus.NOT_FOUND.code()) {
+                        log.warn("Not found: ${formatUrl(query)}")
+                        future.complete(null)
+                    } else {
+                        log.error("Failed to invoke ${formatUrl(query)} status code ${response.statusCode()}")
+                        future.completeExceptionally(Exception(response.statusMessage()))
                     }
-                    response.exceptionHandler { e -> future.completeExceptionally(e) }
-                } else if (response.statusCode() == HttpResponseStatus.NOT_FOUND.code()) {
-                    log.warn("Not found: ${formatUrl(query)}")
-                    future.complete(null)
                 } else {
-                    log.error("Failed to invoke ${formatUrl(query)} status code ${response.statusCode()}")
-                    future.completeExceptionally(Exception(response.statusMessage()))
+                    future.completeExceptionally(queryResult.cause())
                 }
-            }.end()
+
+            }
+        log.info("Query: ${formatUrl(query)}")
+
         return future
     }
 
